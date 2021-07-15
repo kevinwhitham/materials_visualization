6906f5b6f6270f3365d7042a5df31433824f2344
kwhitham <kwhitham@lbl.gov>
2021-07-15 15:39:47 -0500

    add trajectories, xz view, unitcell, and orthographic

--- materials_visualization.py
+++ materials_visualization.py
@@ -1,14 +1,32 @@
 import nglview
+import math
 from ipywidgets import HBox, VBox, Label
 
-def show_ngl_row(mols, show_indices=True, captions=None):
+def show_ngl_row(mols, show_indices=False, captions=None, trajectories=False):
     full_width = 1500
     w = full_width//len(mols)
-    views = [nglview.show_ase(mol) for mol in mols]
+    if trajectories:
+        views = [nglview.show_asetraj(mol) for mol in mols]
+    else:
+        views = [nglview.show_ase(mol) for mol in mols]
+
     for view in views:
+
+        # Add indices to all atoms
         if show_indices:
             view.add_label(labelType='atomindex', color='black')
+
+        # Set width of each view
         view._remote_call('setSize', target='Widget', args=[f'{w}px',f'400px'])
+
+        # We usually want to view the xz plane of a unit cell
+        view.control.spin([1, 0, 0], math.pi/2)
+
+        # Add axes
+        view.add_representation(repr_type='unitcell')
+
+        # Set view to orthographic
+        view.camera = 'orthographic'
     
     result = HBox(views)
     
